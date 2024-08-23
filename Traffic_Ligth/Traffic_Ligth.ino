d6405e0ec0c9c694a64d0e03bf0ed56a29d24d03
Harold Zambrano <hdzambrano.2350@unicesmag.edu.co>
2024-08-22 21:31:18 -0500

    Script:Sketch_Traffic_Ligth

---
+++ Traffic_Ligth/Traffic_Ligth.ino
@@ -0,0 +1,37 @@
+
+const int pinRojo = 13;
+const int pinAmarillo = 12;
+const int pinVerde = 11;
+
+void setup() {
+  // Configurar los pines de los LEDs como salidas
+  pinMode(pinRojo, OUTPUT);
+  pinMode(pinAmarillo, OUTPUT);
+  pinMode(pinVerde, OUTPUT);
+}
+
+void loop() {
+  
+  digitalWrite(pinRojo, HIGH);
+  digitalWrite(pinAmarillo, LOW);
+  digitalWrite(pinVerde, LOW);
+  delay(5000); 
+  
+
+  digitalWrite(pinRojo, LOW);
+  digitalWrite(pinAmarillo, HIGH);
+  digitalWrite(pinVerde, LOW);
+  delay(2000); 
+  
+ 
+  digitalWrite(pinRojo, LOW);
+  digitalWrite(pinAmarillo, LOW);
+  digitalWrite(pinVerde, HIGH);
+  delay(5000); 
+  
+
+  digitalWrite(pinRojo, LOW);
+  digitalWrite(pinAmarillo, HIGH);
+  digitalWrite(pinVerde, LOW);
+  delay(2000); 
+}
